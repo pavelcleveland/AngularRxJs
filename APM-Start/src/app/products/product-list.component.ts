3bacfecb12dec119c424e9543401b3792e42e121
Pavel @ MBP <pavel@pavelpopov.com>
2020-07-21 21:56:38 -0400

    product category filter through combineLatest and Subject

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 
-import { Subscription, Observable, EMPTY } from 'rxjs';
+import { Subscription, Observable, EMPTY, Subject, combineLatest, BehaviorSubject } from 'rxjs';
 
 import { Product } from './product';
 import { ProductService } from './product.service';
-import { catchError } from 'rxjs/operators';
+import { catchError, map } from 'rxjs/operators';
+import { ProductCategoryService } from '../product-categories/product-category.service';
 
 @Component({
   templateUrl: './product-list.component.html',
@@ -14,15 +15,29 @@ import { catchError } from 'rxjs/operators';
 export class ProductListComponent implements OnInit {
   pageTitle = 'Product List';
   errorMessage = '';
-  categories;
+  
+  private categorySelectedSubject = new BehaviorSubject<number>(0);
+  categorySelectedAction$ = this.categorySelectedSubject.asObservable();
 
-  products$ = this.productService.productsWithCategory$
-    .pipe(catchError(err => {
+  categories$ = this.productCategoryService.productCategories$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
+
+  products$ = combineLatest([this.productService.productsWithCategory$, this.categorySelectedAction$]) 
+    .pipe(
+      map(([products, selectedCategoryId]) =>
+        products.filter(product => selectedCategoryId ? product.categoryId === selectedCategoryId : true)),
+      catchError(err => {
       this.errorMessage = err;
       return EMPTY;
     }));
+  
 
-  constructor(private productService: ProductService) { }
+  constructor(private productService: ProductService, private productCategoryService: ProductCategoryService) { }
 
   ngOnInit(): void {
   }
@@ -32,6 +47,6 @@ export class ProductListComponent implements OnInit {
   }
 
   onSelected(categoryId: string): void {
-    console.log('Not yet implemented');
+    this.categorySelectedSubject.next(+categoryId);
   }
 }
