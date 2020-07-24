70a897f30e4ce7d739dac02db6fdee909231b411
Pavel @ MBP <pavel@pavelpopov.com>
2020-07-24 10:09:45 -0400

    product category for alt

--- APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
+++ APM-Start/src/app/products/product-list-alt/product-list-alt.component.ts
@@ -15,7 +15,7 @@ export class ProductListAltComponent implements OnInit, OnDestroy {
   errorMessage = '';
   selectedProductId: number;
 
-  products$ = this.productService.products$
+  products$ = this.productService.productsWithCategory$
     .pipe(catchError(err => {
       this.errorMessage = err;
       return EMPTY;
